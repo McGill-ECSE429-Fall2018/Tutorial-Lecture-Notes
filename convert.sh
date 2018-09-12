da4adffb4df6e14b475f81918b9fd3039dcfc718
Marton Bur <marton.bur@gmail.com>
2018-09-12 09:08:08 -0400

    Fixing filename in convert script

--- convert.sh
+++ convert.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-asciidoctor -b html5 ECSE-321-Tutorials.adoc -o index.html
-asciidoctor-pdf ECSE-321-Tutorials.adoc
+asciidoctor -b html5 ECSE-429-Tutorials.adoc -o index.html
+asciidoctor-pdf ECSE-429-Tutorials.adoc
